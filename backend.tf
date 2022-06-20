e1d407199533675e81cf1ba0ad3379c369ddd5d9
Jesus Molina <mmjesu6@mapfre.com>
2022-06-20 07:52:18 +0200

    fix: deleting wrong characters

--- backend.tf
+++ backend.tf
@@ -2,8 +2,4 @@ terraform {
   backend "local" { 
       path= "/opt/buckets/mmjesu6"
   }
-
 }
-~                                                                                                                                                                                   
-~                                                                                                                                                                                   
-~                   
